f9a6503de2315fb14e6aed9692a326536945618d
wojdzie <wojdzi@outlook.com>
2021-04-17 15:31:31 +0200

    Heap sort has been changed

--- src/algorithm/HeapSort.java
+++ src/algorithm/HeapSort.java
@@ -1,17 +1,40 @@
 package algorithm;
 
-import datastructure.PriorityQueue;
+public class HeapSort {
 
-public class HeapSort<E extends Comparable<E>> {
-
-    public static <E> void sort(Comparable[] array) {
-        PriorityQueue priorityQueue = new PriorityQueue<>(array.length);
-        for (Comparable item : array) {
-            priorityQueue.insert(item);
+    /*
+        Heap sort is based on heap so position 0 should be empty
+     */
+    public static void sort(Comparable[] array) {
+        int size = array.length - 1;
+        for (int i = size / 2; i >= 1; i--) {
+            sink(array, i, size);
         }
+        while (size > 1) {
+            Comparable temp = array[1];
+            array[1] = array[size];
+            array[size] = temp;
+            size--;
 
-        for (int i = array.length - 1; i >= 0; i--) {
-            array[i] = priorityQueue.delMax();
+            sink(array, 1, size);
+        }
+    }
+
+    private static void sink(Comparable[] array, int position, int size) {
+        while (position * 2 <= size) {
+            int childPosition = position * 2;
+            if (childPosition < size && array[childPosition + 1].compareTo(array[childPosition]) > 0) {
+                childPosition++;
+            }
+            if (array[position].compareTo(array[childPosition]) > 0) {
+                break;
+            }
+
+            Comparable temp = array[position];
+            array[position] = array[childPosition];
+            array[childPosition] = temp;
+
+           position = childPosition;
         }
     }
 }
