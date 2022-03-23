758fa568055c0dadd9af9cf126896534d5565210
wojdzie <wojdzi@outlook.com>
2022-03-23 08:05:34 +0100

    Linked lists have been implemented

--- src/datastructure/LinkedList.java
+++ src/datastructure/LinkedList.java
@@ -3,6 +3,7 @@ package datastructure;
 public class LinkedList<E> {
 
      private static class SinglyLinkedListImplementation<E> {
+
           private Node<E> head;
           private int size = 0;
 
@@ -11,41 +12,48 @@ public class LinkedList<E> {
                E value;
           }
 
-          public void add(E value) {
+          public void push(E value) {
+               Node<E> newNode = new Node<>();
+               newNode.nextNode = head;
+               newNode.value = value;
+               head = newNode;
+               size++;
+          }
+
+          public void append(E value) {
+               Node<E> newNode = new Node<>();
+               newNode.value = value;
                if (head == null) {
-                    Node<E> newNode = new Node<>();
-                    newNode.value = value;
                     head = newNode;
-                    size++;
-               } else {
-                    Node<E> current = head;
-                    while (current.nextNode != null) {
-                         current = current.nextNode;
-                    }
-                    Node<E> newNode = new Node<>();
-                    newNode.value = value;
-                    current.nextNode = newNode;
-                    size++;
+                    return;
                }
+               Node<E> lastNode = head;
+               while (lastNode.nextNode != null) {
+                    lastNode = lastNode.nextNode;
+               }
+               lastNode.nextNode = newNode;
+               size++;
           }
 
           public void remove(E value) {
-               if (head != null) {
-                    Node<E> previous = null;
-                    Node<E> current = head;
-                    while (!current.value.equals(value) && current.nextNode != null) {
-                         previous = current;
-                         current = current.nextNode;
-                    }
-                    if (current.value.equals(value)) {
-                         if (previous == null) {
-                              head = current.nextNode;
-                         } else {
-                              previous.nextNode = current.nextNode;
-                         }
-                         size--;
-                    }
+               if (head == null) {
+                    return;
                }
+               Node<E> previousNode = null;
+               Node<E> searchedNode = head;
+               while (!searchedNode.value.equals(value) && searchedNode.nextNode != null) {
+                    previousNode = searchedNode;
+                    searchedNode = searchedNode.nextNode;
+               }
+               if (!searchedNode.value.equals(value)) {
+                    return;
+               }
+               if (previousNode == null) {
+                    head = searchedNode.nextNode;
+               } else {
+                    previousNode.nextNode = searchedNode.nextNode;
+               }
+               size--;
           }
 
           public boolean isEmpty() {
@@ -57,7 +65,92 @@ public class LinkedList<E> {
           }
      }
 
+     private static class CircularLinkedListImplementation<E> {
+
+          private Node<E> last;
+          private int size = 0;
+
+          private static class Node<E> {
+               Node<E> nextNode;
+               E value;
+          }
+
+          public void push(E value) {
+               if (last == null) {
+                    Node<E> newNode = new Node<>();
+                    newNode.value = value;
+                    last = newNode;
+                    last.nextNode = last;
+               } else {
+                    Node<E> newNode = new Node<>();
+                    newNode.value = value;
+                    newNode.nextNode = last.nextNode;
+                    last.nextNode = newNode;
+               }
+               size++;
+          }
+          public void append(E value) {
+               if (last == null) {
+                    Node<E> newNode = new Node<>();
+                    newNode.value = value;
+                    last = newNode;
+                    last.nextNode = last;
+               } else {
+                    Node<E> newNode = new Node<>();
+                    newNode.value = value;
+                    newNode.nextNode = last.nextNode;
+                    last.nextNode = newNode;
+                    last = newNode;
+               }
+               size++;
+          }
+          public void remove(E value) {
+               if (last == null) {
+                    return;
+               }
+               Node<E> prevNode = null;
+               Node<E> searchedNode = last;
+               while (!searchedNode.value.equals(value)) {
+                    // Break after iterating whole list
+                    if (searchedNode.nextNode.equals(last)) {
+                         break;
+                    }
+                    prevNode = searchedNode;
+                    searchedNode = searchedNode.nextNode;
+               }
+               // Check if node with given value is found
+               if (!searchedNode.value.equals(value)) {
+                    return;
+               }
+               // Check if found node is single node
+               if (searchedNode.equals(searchedNode.nextNode)) {
+                    last = null;
+               } else {
+                    // Check if found node is last node
+                    if (searchedNode.equals(last)) {
+                         // Find previous node
+                         searchedNode = searchedNode.nextNode;
+                         while (!searchedNode.equals(last)) {
+                              prevNode = searchedNode;
+                              searchedNode = searchedNode.nextNode;
+                         }
+                         last = searchedNode.nextNode;
+                    }
+                    prevNode.nextNode = searchedNode.nextNode;
+               }
+               size--;
+          }
+
+          public boolean isEmpty() {
+               return size == 0;
+          }
+          public int size() {
+               return size;
+          }
+     }
+
      private static class DoublyLinkedListImplementation<E> {
+
           private Node<E> head;
           private int size = 0;
 
@@ -67,47 +160,58 @@ public class LinkedList<E> {
                E value;
           }
 
-          public void add(E value) {
-               if (head == null) {
-                    Node<E> newNode = new Node<>();
-                    newNode.value = value;
-                    head = newNode;
-                    size++;
-               } else {
-                    Node<E> current = head;
-                    while (current.nextNode != null) {
-                         current = current.nextNode;
-                    }
-                    Node<E> newNode = new Node<>();
-                    newNode.prevNode = current;
-                    newNode.value = value;
-                    current.nextNode = newNode;
-                    size++;
+          public void push(E value) {
+               Node<E> newNode = new Node<>();
+               newNode.value = value;
+               newNode.nextNode = head;
+               if (head != null) {
+                    head.prevNode = newNode;
                }
+               head = newNode;
+               size++;
+          }
+
+          public void append(E value) {
+               Node<E> newNode = new Node<>();
+               newNode.value = value;
+               if (head == null) {
+                    head = newNode;
+                    return;
+               }
+               Node<E> lastNode = head;
+               while (lastNode.nextNode != null) {
+                    lastNode = lastNode.nextNode;
+               }
+               lastNode.nextNode = newNode;
+               newNode.prevNode = lastNode;
+               size++;
           }
 
           public void remove(E value) {
-               if (head != null) {
-                    Node<E> current = head;
-                    while (!current.value.equals(value) && current.nextNode != null) {
-                         current = current.nextNode;
+               if (head == null) {
+                    return;
+               }
+               Node<E> searchedNode = head;
+               while (!searchedNode.value.equals(value) && searchedNode.nextNode != null) {
+                    searchedNode = searchedNode.nextNode;
+               }
+               if (!searchedNode.value.equals(value)) {
+                    return;
+               }
+               Node<E> nextNode = searchedNode.nextNode;
+               if (searchedNode.prevNode == null) {
+                    head = nextNode;
+                    if (nextNode != null) {
+                         nextNode.prevNode = null;
                     }
-                    if (current.value.equals(value)) {
-                         Node<E> previous = current.prevNode;
-                         Node<E> next = current.nextNode;
-                         if (previous == null) {
-                              head = next;
-                         } else {
-                              previous.nextNode = next;
-                         }
-                         if (next == null) {
-                              previous.nextNode = null;
-                         } else {
-                              next.prevNode = previous;
-                         }
-                         size--;
+               } else {
+                    Node<E> prevNode = searchedNode.prevNode;
+                    prevNode.nextNode = nextNode;
+                    if (nextNode != null) {
+                         nextNode.prevNode = prevNode;
                     }
                }
+               size--;
           }
 
           public boolean isEmpty() {
@@ -117,30 +221,39 @@ public class LinkedList<E> {
           public int size() {
                return size;
           }
-
-
      }
 
      public static void main(String[] args) {
           SinglyLinkedListImplementation<Integer> singlyLinkedListImplementation = new SinglyLinkedListImplementation<>();
-          singlyLinkedListImplementation.add(20);
-          singlyLinkedListImplementation.add(17);
-          singlyLinkedListImplementation.add(11);
-          singlyLinkedListImplementation.add(18);
-          singlyLinkedListImplementation.add(21);
-          singlyLinkedListImplementation.remove(20);
-          singlyLinkedListImplementation.remove(11);
-          singlyLinkedListImplementation.remove(21);
+          singlyLinkedListImplementation.push(10);
+          singlyLinkedListImplementation.push(7);
+          singlyLinkedListImplementation.push(3);
+          singlyLinkedListImplementation.append(1);
+          singlyLinkedListImplementation.append(11);
+          singlyLinkedListImplementation.append(8);
+          singlyLinkedListImplementation.remove(3);
+          singlyLinkedListImplementation.remove(8);
+          singlyLinkedListImplementation.remove(10);
 
           DoublyLinkedListImplementation<Integer> doublyLinkedListImplementation = new DoublyLinkedListImplementation<>();
-          doublyLinkedListImplementation.add(10);
-          doublyLinkedListImplementation.add(7);
-          doublyLinkedListImplementation.add(1);
-          doublyLinkedListImplementation.add(8);
-          doublyLinkedListImplementation.add(11);
-          doublyLinkedListImplementation.remove(10);
-          doublyLinkedListImplementation.remove(1);
-          doublyLinkedListImplementation.remove(11);
+          doublyLinkedListImplementation.push(20);
+          doublyLinkedListImplementation.push(17);
+          doublyLinkedListImplementation.push(13);
+          doublyLinkedListImplementation.append(11);
+          doublyLinkedListImplementation.append(21);
+          doublyLinkedListImplementation.append(18);
+          doublyLinkedListImplementation.remove(13);
+          doublyLinkedListImplementation.remove(18);
+          doublyLinkedListImplementation.remove(20);
+
+          CircularLinkedListImplementation<Integer> circularLinkedListImplementation = new CircularLinkedListImplementation<>();
+          circularLinkedListImplementation.push(5);
+          circularLinkedListImplementation.push(3);
+          circularLinkedListImplementation.append(11);
+          circularLinkedListImplementation.append(9);
+          circularLinkedListImplementation.remove(5);
+          circularLinkedListImplementation.remove(3);
+          circularLinkedListImplementation.remove(9);
      }
 }
 
