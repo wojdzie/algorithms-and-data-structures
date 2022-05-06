2b3710d769b9ed252b8317dd5aa8e8c26b3483ce
wojdzie <wojdzi@outlook.com>
2022-05-06 19:15:45 +0200

    Simplified Merge Sort

--- src/algorithm/MergeSort.java
+++ src/algorithm/MergeSort.java
@@ -2,46 +2,59 @@ package algorithm;
 
 public class MergeSort {
 
+    private static class MergeSortImplementation {
 
-    public static void sort(Comparable[] array) {
-        sort(array, 0, array.length - 1);
+        public static void sort(int[] array) {
+            sort(array, new int[array.length], 0, array.length - 1);
+        }
+
+        private static void sort(int[] array, int[] temp, int lo, int hi) {
+            if (lo < hi) {
+                int mid = lo + (hi - lo) / 2;
+                sort(array, temp, lo, mid);
+                sort(array, temp, mid + 1, hi);
+                merge(array, temp, lo, hi);
+            }
+        }
+
+        private static void merge(int[] array, int[] temp, int lo, int hi) {
+            int leftStart = lo;
+            int leftEnd = lo + (hi - lo) / 2;
+            int rightStart = leftEnd + 1;
+            int rightEnd = hi;
+            int index = leftStart;
+            // Merge elements
+            while (leftStart <= leftEnd && rightStart <= rightEnd) {
+                if (array[leftStart] <= array[rightStart]) {
+                    temp[index] = array[leftStart];
+                    leftStart++;
+                } else {
+                    temp[index] = array[rightStart];
+                    rightStart++;
+                }
+                index++;
+            }
+            // Copy remaining elements from left array
+            while (leftStart <= leftEnd) {
+                temp[index] = array[leftStart];
+                leftStart++;
+                index++;
+            }
+            // Copy remaining elements from right array
+            while (rightStart <= rightEnd) {
+                temp[index] = array[rightStart];
+                rightStart++;
+                index++;
+            }
+            // Copy all elements to main array
+            for (int i = lo; i <= hi; i++) {
+                array[i] = temp[i];
+            }
+        }
     }
 
-    private static void sort(Comparable[] array, int lo, int hi) {
-        if (lo < hi) {
-            int mid = (lo + hi) / 2;
-            sort(array, lo, mid);
-            sort(array, mid + 1, hi);
-            merge(array, lo, mid, hi);
-        }
-    }
-
-    private static void merge(Comparable[] array, int lo, int mid, int hi) {
-        Comparable[] temp = new Comparable[array.length];
-        for (int i = 0; i < temp.length; i++) {
-            temp[i] = array[i];
-        }
-
-        int i = lo;
-        int j = mid + 1;
-        int current = lo;
-        while (i <= mid && j <= hi) {
-            if (temp[i].compareTo(temp[j]) < 0) {
-                array[current++] = temp[i++];
-            } else {
-                array[current++] = temp[j++];
-            }
-        }
-        if (i <= mid && j > hi) {
-            while (i <= mid) {
-                array[current++] = temp[i++];
-            }
-        }
-
-        if (i > mid && j <= hi) {
-            while (j <= hi) {
-                array[current++] = temp[j++];
-            }
-        }
+    public static void main(String[] args) {
+        int[] array = new int[] {9, 1, 2, 4, 5, 7, 8, 6, 3};
+        MergeSort.MergeSortImplementation.sort(array);
     }
 }
